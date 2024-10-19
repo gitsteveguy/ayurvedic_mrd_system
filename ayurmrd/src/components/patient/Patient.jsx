2df9281bdb12b79e72406237a7cab004fe8466e0
Steve SajanJacob <121501747+gitsteveguy@users.noreply.github.com>
2024-10-19 20:04:13 +0530

    Added Export Functionality for Patient Visit Records

--- ayurmrd/src/components/patient/Patient.jsx
+++ ayurmrd/src/components/patient/Patient.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Container from '../Container'
 import './patient.css'
 import axios from 'axios';
@@ -13,98 +13,101 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Patient = (props) => {
-    const patient_id = getCurrentPatientID();
-    const current_user = useAuth();
-    const navigate = useNavigate()
-    const [patientData,setPatientData] = useState({
-        address_line_1: "",
-        address_line_2: "",
-        blood_group: "",
-        country: "",
-        country_code: "",
-        date_of_birth: "",
-        email: "",
-        first_name: "",
-        gender: "",
-        last_name: "",
-        occupation: "",
-        phone_no: "",
-        pincode: "",
-        profile_img: "",
-        role: "",
-        signature_img: "",
-        state: "",
-        user_id: "",
-        username: ""
-      })
-      function calculateAge(dateOfBirth) {
-        const dob = new Date(dateOfBirth);
-        const now = new Date();
-        
-        let age = now.getFullYear() - dob.getFullYear();
-        
-        // Adjust age if birthday hasn't occurred yet this year
-        if (now.getMonth() < dob.getMonth() || (now.getMonth() === dob.getMonth() && now.getDate() < dob.getDate())) {
-          age--;
-        }
-        
-        return age;
-      }
+  const patient_id = getCurrentPatientID();
+  const current_user = useAuth();
+  const navigate = useNavigate()
+  const [patientData, setPatientData] = useState({
+    address_line_1: "",
+    address_line_2: "",
+    blood_group: "",
+    country: "",
+    country_code: "",
+    date_of_birth: "",
+    email: "",
+    first_name: "",
+    gender: "",
+    last_name: "",
+    occupation: "",
+    phone_no: "",
+    pincode: "",
+    profile_img: "",
+    role: "",
+    signature_img: "",
+    state: "",
+    user_id: "",
+    username: ""
+  })
+  function calculateAge(dateOfBirth) {
+    const dob = new Date(dateOfBirth);
+    const now = new Date();
 
-      function extractDateOfBirth(dateOfBirth) {
-        const dob = new Date(dateOfBirth);
-        return dob.toDateString(); // Example: "Tue Jun 10 2003"
-      }
+    let age = now.getFullYear() - dob.getFullYear();
 
-    const patient_data_api_url = 'http://localhost:5000/api/get_patients_by_id';
-    
-    useEffect(() => {
-        axios.get(patient_data_api_url,{params: {id: patient_id}, withCredentials: true}).then(response => {
-            if (!response) {
-              throw new Error('Network response was not ok');
-            }
-            return (response.data);
-          }).then((data) => {
-            setPatientData(data);
-          })
-      }, [patient_data_api_url]);
-    
+    // Adjust age if birthday hasn't occurred yet this year
+    if (now.getMonth() < dob.getMonth() || (now.getMonth() === dob.getMonth() && now.getDate() < dob.getDate())) {
+      age--;
+    }
+
+    return age;
+  }
+
+  function extractDateOfBirth(dateOfBirth) {
+    const dob = new Date(dateOfBirth);
+    return dob.toDateString(); // Example: "Tue Jun 10 2003"
+  }
+
+  const patient_data_api_url = 'http://localhost:5000/api/get_patients_by_id';
+
+  useEffect(() => {
+    axios.get(patient_data_api_url, { params: { id: patient_id }, withCredentials: true }).then(response => {
+      if (!response) {
+        throw new Error('Network response was not ok');
+      }
+      return (response.data);
+    }).then((data) => {
+      setPatientData(data);
+    })
+  }, [patient_data_api_url]);
+
 
   return (
     <Container page_name={props.title} active_menu='Patients' type='flex'>
-        <div className="card">
-            <div id="detailContainer">
-            <img src={patientData.profile_img} alt="" />
-            <div className="patient-details">
-            <h3>Name : {patientData.first_name+' '+patientData.last_name}</h3>
-            <h3><EmailIcon fontSize='small' />  <a href={"mailto:"+patientData.email}>{patientData.email}</a></h3>
-            <h3><CallIcon fontSize='small'/>  <a href={'tel:'+patientData.phone_no}>{patientData.phone_no}</a></h3>
+      <div className="card">
+        <div id="detailContainer">
+          <img src={patientData.profile_img} alt="" />
+          <div className="patient-details">
+            <h3>Name : {patientData.first_name + ' ' + patientData.last_name}</h3>
+            <h3><EmailIcon fontSize='small' />  <a href={"mailto:" + patientData.email}>{patientData.email}</a></h3>
+            <h3><CallIcon fontSize='small' />  <a href={'tel:' + patientData.phone_no}>{patientData.phone_no}</a></h3>
             <h3>Nationality : {patientData.country}</h3>
-            </div>
-            <div className="patient-details">
+          </div>
+          <div className="patient-details">
             <h3>Gender : {patientData.gender}</h3>
-            <h3>< BloodtypeIcon fontSize='small' style={{color: 'red'}}/> {patientData.blood_group}</h3>
+            <h3>< BloodtypeIcon fontSize='small' style={{ color: 'red' }} /> {patientData.blood_group}</h3>
             <h3>Date of Birth : {extractDateOfBirth(patientData.date_of_birth)}</h3>
             <h3>Age : {calculateAge(patientData.date_of_birth)}</h3>
-            </div>
-            </div>
-             <div className="container-top-btn-grp" style={{margin:'0.5rem'}}>
-            <a className='primary-btn' onClick={() => navigate(-1)} style={{borderRadius:'24px',cursor:'pointer'}}><ArrowBackIosNewIcon/>Go Back</a>
-          {props.hBtns && props.hBtns.map((btn,index)=>{
-            let borderRadius='24px';
-            if(!btn.text)
-              borderRadius='50%'
-            if(current_user.permissions.includes(btn.permission))
-              return(
-              <a key={index} href={btn.href} className={btn.className} style={{borderRadius:borderRadius}} tooltip={btn.tooltip}>{btn.icon}{btn.text}</a>)
+          </div>
+        </div>
+        <div className="container-top-btn-grp" style={{ margin: '0.5rem' }}>
+          <a className='primary-btn' onClick={() => navigate(-1)} style={{ borderRadius: '24px', cursor: 'pointer' }}><ArrowBackIosNewIcon />Go Back</a>
+          {props.hBtns && props.hBtns.map((btn, index) => {
+            let borderRadius = '24px';
+            if (!btn.text)
+              borderRadius = '50%'
+            if (!Array.isArray(btn.permission)) {
+              btn.permission = [btn.permission];
+            }
+            if (btn.permission.some(each_permission => current_user.permissions.includes(each_permission)))
+              return (
+                <a key={index} href={btn.href} onClick={btn.onClick} className={btn.className} style={{ borderRadius: borderRadius }} tooltip={btn.tooltip}>{btn.icon}{btn.text}</a>)
           })
           }
 
         </div>
-        </div>
-        {props.children}
-  
-      
+      </div>
+      {props.children}
+
+
 
     </Container>
   )
--- ayurmrd/src/hooks/currentPatientnVisit.jsx
+++ ayurmrd/src/hooks/currentPatientnVisit.jsx
@@ -1,23 +1,23 @@
-export const setCurrentPatientID = (id)=>{
- window.localStorage.setItem('current_patient_id',id);
+export const setCurrentPatientID = (id) => {
+    window.localStorage.setItem('current_patient_id', id);
 }
-export const getCurrentPatientID = ()=>{
-    try{
-    return window.localStorage.getItem('current_patient_id');
+export const getCurrentPatientID = () => {
+    try {
+        return window.localStorage.getItem('current_patient_id');
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 }
 
-export const setCurrentPatientVisitID = (id)=>{
-    window.localStorage.setItem('current_patient_visit_id',id);
-   }
-export const getCurrentPatientVisitID = ()=>{
-       try{
-       return window.localStorage.getItem('current_patient_visit_id');
-       }
-       catch(err){
-           console.log(err);
-       }
-   }
+export const setCurrentPatientVisitID = (id) => {
+    window.localStorage.setItem('current_patient_visit_id', id);
+}
+export const getCurrentPatientVisitID = () => {
+    try {
+        return window.localStorage.getItem('current_patient_visit_id');
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
